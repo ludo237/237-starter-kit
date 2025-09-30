2dbe1f614a2837e8cbf921dab458d9d7d2e96ed0
Claudio Ludovico <921500+ludo237@users.noreply.github.com>
2025-09-30 18:18:45 +0200

    chore: add sensible defaults in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,23 +4,21 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        URL::forceHttps($this->app->environment(['production', 'prod']));
+
+        Date::use(CarbonImmutable::class);
+
+        Vite::useAggressivePrefetching();
+        Vite::prefetch();
     }
 }
